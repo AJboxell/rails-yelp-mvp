d944ce597a1f27695f9409b76251147d04c36273
Alex Boxell <alexjboxell@gmail.com>
2021-11-10 11:51:38 +0000

    basic functionality all present

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+puts "Removing existing restaurants..."
+
+10.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  puts "Creating restaurant named #{restaurant.name}"
+  restaurant.save
+end
